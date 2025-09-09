378afb8d37e9ceb1732fd25471fc42662ce67b00
pscanf <paolo.scanferla@gmail.com>
2025-09-09 10:36:58 +0300

    fix: build

--- packages/core/executing-backend/src/ExecutingBackend.test.ts
+++
@@ -1,7 +0,0 @@
-import { registerBackendTests } from "@superego/backend/tests";
-import ExecutingBackend from "./ExecutingBackend.js";
-
-registerBackendTests(async () => {
-  const backend = new ExecutingBackend();
-  return { backend };
-});
