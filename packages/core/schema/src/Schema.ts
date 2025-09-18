e80e9ac19427152c55af4cbbb78e422d7b6bdb50
pscanf <paolo.scanferla@gmail.com>
2025-09-18 15:21:10 +0300

    feat: added comment to Schema d.ts

--- packages/core/schema/src/Schema.ts
+++ packages/core/schema/src/Schema.ts
@@ -1,8 +1,16 @@
 import type { AnyTypeDefinition } from "./typeDefinitions.js";
 
 export default interface Schema {
+  /**
+   * Record (by name) of type definitions. Define complex types here once and
+   * reuse them elsewhere in the schema.
+   */
   types: {
     [name: string]: AnyTypeDefinition;
   };
+  /**
+   * Ref to the type that defines the overall structure of the document. Must
+   * exist in `types`. Must be a `StructTypeDefinition`.
+   */
   rootType: string;
 }
