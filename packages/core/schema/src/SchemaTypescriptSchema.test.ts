bcfb197226fdbf8c41fb83600eb5bc3a9276d38a
pscanf <paolo.scanferla@gmail.com>
2025-09-18 15:29:25 +0300

    fix: test

--- packages/core/schema/src/SchemaTypescriptSchema.test.ts
+++ packages/core/schema/src/SchemaTypescriptSchema.test.ts
@@ -147,9 +147,17 @@ it("exports the Schema type definition", () => {
       | TypeDefinitionRef;
 
     interface Schema {
+      /**
+       * Record (by name) of type definitions. Define complex types here once and
+       * reuse them elsewhere in the schema.
+       */
       types: {
         [name: string]: AnyTypeDefinition;
       };
+      /**
+       * Ref to the type that defines the overall structure of the document. Must
+       * exist in \`types\`. Must be a \`StructTypeDefinition\`.
+       */
       rootType: string;
     }
     "
