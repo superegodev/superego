40aac62366111311e563b0e4f55b5e8a00c583f8
pscanf <paolo.scanferla@gmail.com>
2025-10-02 23:36:08 -0400

    test: some more collections e2e

--- packages/tests/backend-e2e-tests/src/suites/collections-down-sync.ts
+++ packages/tests/backend-e2e-tests/src/suites/collections-down-sync.ts
@@ -138,6 +138,158 @@ export default rd<GetDependencies>("Collections down-sync", (deps) => {
         ),
       );
     });
+
+    it("success: syncs remote changes (case: subsequent sync)", async () => {
+      // Setup mocks
+      const firstChanges: Connector.Changes = {
+        addedOrModified: [
+          {
+            id: "remoteId1",
+            versionId: "remoteVersionId1.0",
+            content: { title: "title1" },
+          },
+          {
+            id: "remoteId2",
+            versionId: "remoteVersionId2.0",
+            content: { title: "title2" },
+          },
+        ],
+        deleted: [],
+      };
+      const secondChanges: Connector.Changes = {
+        addedOrModified: [
+          {
+            id: firstChanges.addedOrModified[0]!.id,
+            versionId: "remoteVersionId1.1",
+            content: { title: "updated-title1" },
+          },
+          {
+            id: "remoteId3",
+            versionId: "remoteVersionId3.0",
+            content: { title: "title3" },
+          },
+        ],
+        deleted: [{ id: firstChanges.addedOrModified[1]!.id }],
+      };
+      const mockConnector: Connector = {
+        name: "MockConnector",
+        settingsSchema: {
+          types: { Settings: { dataType: DataType.Struct, properties: {} } },
+          rootType: "Settings",
+        },
+        remoteDocumentSchema: {
+          types: {
+            RemoteDocument: {
+              dataType: DataType.Struct,
+              properties: { title: { dataType: DataType.String } },
+            },
+          },
+          rootType: "RemoteDocument",
+        },
+        syncDown: async (syncFrom) => ({
+          success: true,
+          data:
+            syncFrom === null
+              ? { changes: firstChanges, syncPoint: "syncPoint1" }
+              : { changes: secondChanges, syncPoint: "syncPoint2" },
+          error: null,
+        }),
+      };
+
+      // Setup SUT
+      const { backend } = deps(mockConnector);
+      const createCollectionResult = await backend.collections.create(
+        {
+          name: "name",
+          icon: null,
+          collectionCategoryId: null,
+          description: null,
+          assistantInstructions: null,
+        },
+        {
+          types: {
+            Root: {
+              dataType: DataType.Struct,
+              properties: { title: { dataType: DataType.String } },
+            },
+          },
+          rootType: "Root",
+        },
+        {
+          contentSummaryGetter: {
+            source: "",
+            compiled:
+              "export default function getContentSummary() { return {}; }",
+          },
+        },
+      );
+      assert.isTrue(createCollectionResult.success);
+      const setRemoteResult = await backend.collections.setRemote(
+        createCollectionResult.data.id,
+        mockConnector.name,
+        {},
+        {
+          fromRemoteDocument: {
+            source: "",
+            compiled:
+              "export default function fromRemoteDocument(remote) { return { title: remote.title }; }",
+          },
+        },
+      );
+      assert.isTrue(setRemoteResult.success);
+
+      // Exercise first downSync
+      await triggerAndWaitForDownSync(backend, createCollectionResult.data.id);
+
+      // Verify first downSync
+      const firstDocumentsListResult = await backend.documents.list(
+        createCollectionResult.data.id,
+      );
+      assert.isTrue(firstDocumentsListResult.success);
+      expect(firstDocumentsListResult.data).toHaveLength(2);
+      expect(firstDocumentsListResult.data).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            remoteId: firstChanges.addedOrModified[0]!.id,
+            latestVersion: expect.objectContaining({
+              remoteId: firstChanges.addedOrModified[0]!.versionId,
+            }),
+          }),
+          expect.objectContaining({
+            remoteId: firstChanges.addedOrModified[1]!.id,
+            latestVersion: expect.objectContaining({
+              remoteId: firstChanges.addedOrModified[1]!.versionId,
+            }),
+          }),
+        ]),
+      );
+
+      // Exercise second downSync
+      await triggerAndWaitForDownSync(backend, createCollectionResult.data.id);
+
+      // Verify second downSync
+      const secondDocumentsListResult = await backend.documents.list(
+        createCollectionResult.data.id,
+      );
+      assert.isTrue(secondDocumentsListResult.success);
+      expect(secondDocumentsListResult.data).toHaveLength(2);
+      expect(secondDocumentsListResult.data).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            remoteId: secondChanges.addedOrModified[0]!.id,
+            latestVersion: expect.objectContaining({
+              remoteId: secondChanges.addedOrModified[0]!.versionId,
+            }),
+          }),
+          expect.objectContaining({
+            remoteId: secondChanges.addedOrModified[1]!.id,
+            latestVersion: expect.objectContaining({
+              remoteId: secondChanges.addedOrModified[1]!.versionId,
+            }),
+          }),
+        ]),
+      );
+    });
   });
 });
 
