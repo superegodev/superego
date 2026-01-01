b78546e242d7c749544b1f5fc8bf5e06157dfc4e
pscanf <paolo.scanferla@gmail.com>
2026-01-01 01:08:30 +0100

    chore: update dependencies

--- packages/apps/browser-app/src/components/design-system/IconLink/IconLink.tsx
+++ packages/apps/browser-app/src/components/design-system/IconLink/IconLink.tsx
@@ -1,13 +1,13 @@
 import type { RefAttributes } from "react";
 import type { Placement } from "react-aria";
 import { TooltipTrigger } from "react-aria-components";
-import type { DistributedOmit } from "react-intl/src/components/injectIntl.js";
 import classnames from "../../../utils/classnames.js";
 import Link from "../Link/Link.js";
 import Tooltip from "../Tooltip/Tooltip.js";
 import * as cs from "./IconLink.css.js";
+import { DistributiveOmit } from "@superego/global-types";
 
-type LinkProps = DistributedOmit<Parameters<typeof Link>[0], "aria-label">;
+type LinkProps = DistributiveOmit<Parameters<typeof Link>[0], "aria-label">;
 
 type Props = LinkProps & {
   variant: "invisible";
