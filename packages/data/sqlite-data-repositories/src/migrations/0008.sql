69f5317e3257ff702e9818f799154d6d6d8d5d8a
pscanf <paolo.scanferla@gmail.com>
2026-02-02 12:06:05 +0800

    refactor: squash migrations

--- packages/data/sqlite-data-repositories/src/migrations/0008.sql
+++ packages/data/sqlite-data-repositories/src/migrations/0008.sql
@@ -20,3 +20,33 @@ CREATE TABLE "conversation_text_search_texts" (
 -- Flexsearch indexes
 
 DROP TABLE "flexsearch_indexes";
+
+-- Document versions
+
+ALTER TABLE "document_versions"
+ADD COLUMN "content_summary" TEXT NOT NULL DEFAULT '{}';
+
+CREATE INDEX "idx__document_versions__on__collection_version_id" ON "document_versions" (
+  "collection_version_id"
+);
+
+-- Collection versions
+
+-- Migrate content_blocking_keys_getter from a separate column into the settings
+-- JSON column.
+
+-- Step 1: Update the settings column to include contentBlockingKeysGetter.
+UPDATE "collection_versions"
+SET "settings" = json_set(
+  "settings",
+  '$.contentBlockingKeysGetter',
+  CASE
+    WHEN "content_blocking_keys_getter" IS NOT NULL THEN json(
+      "content_blocking_keys_getter"
+    )
+    ELSE json('null')
+  END
+);
+
+-- Step 2: Drop the old column.
+ALTER TABLE "collection_versions" DROP COLUMN "content_blocking_keys_getter";
--- packages/data/sqlite-data-repositories/src/migrations/0009.sql
+++
@@ -1,8 +0,0 @@
--- Document versions
-
-ALTER TABLE "document_versions"
-ADD COLUMN "content_summary" TEXT NOT NULL DEFAULT '{}';
-
-CREATE INDEX "idx__document_versions__on__collection_version_id" ON "document_versions" (
-  "collection_version_id"
-);
