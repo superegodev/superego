a64335cf1d446b4254b86f9bdacf3cc7bc4d8cfe
pscanf <paolo.scanferla@gmail.com>
2026-02-16 18:05:28 +0200

    fix: e2e tests

--- packages/misc/bazaar/src/packs/dev/superego/bazaar/productivity/contacts.ts
+++ packages/misc/bazaar/src/packs/dev/superego/bazaar/productivity/contacts.ts
@@ -64,7 +64,7 @@ export default function getContentSummary(contact) {
     },
     defaultDocumentViewUiOptions: {
       fullWidth: true,
-      alwaysCollapsePrimarySidebar: true,
+      alwaysCollapsePrimarySidebar: false,
       rootLayout: {
         "(min-width: 65rem)": [
           {
