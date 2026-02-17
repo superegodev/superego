6adfa96aa1144f649723bd0603f4775997b43b94
pscanf <paolo.scanferla@gmail.com>
2026-02-17 17:17:04 +0200

    feat: support in-memory-only storage for demo-data-repositories

--- packages/data/demo-data-repositories/src/DemoDataRepositoriesManager.test.ts
+++ packages/data/demo-data-repositories/src/DemoDataRepositoriesManager.test.ts
@@ -1,39 +1,80 @@
 import { AssistantName, Theme } from "@superego/backend";
 import { registerDataRepositoriesTests } from "@superego/executing-backend/tests";
+import { describe } from "vitest";
 import DemoDataRepositoriesManager from "./DemoDataRepositoriesManager.js";
 
-registerDataRepositoriesTests(() => {
-  const dataRepositoriesManager = new DemoDataRepositoriesManager(
-    {
-      appearance: { theme: Theme.Auto },
-      inference: {
-        chatCompletions: {
-          provider: { baseUrl: null, apiKey: null },
-          model: null,
+describe("IndexedDB", () => {
+  registerDataRepositoriesTests(() => {
+    const dataRepositoriesManager = new DemoDataRepositoriesManager(
+      {
+        appearance: { theme: Theme.Auto },
+        inference: {
+          chatCompletions: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+          },
+          transcriptions: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+          },
+          speech: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+            voice: null,
+          },
+          fileInspection: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+          },
         },
-        transcriptions: {
-          provider: { baseUrl: null, apiKey: null },
-          model: null,
-        },
-        speech: {
-          provider: { baseUrl: null, apiKey: null },
-          model: null,
-          voice: null,
-        },
-        fileInspection: {
-          provider: { baseUrl: null, apiKey: null },
-          model: null,
+        assistants: {
+          userName: null,
+          developerPrompts: {
+            [AssistantName.Factotum]: null,
+            [AssistantName.CollectionCreator]: null,
+          },
         },
       },
-      assistants: {
-        userName: null,
-        developerPrompts: {
-          [AssistantName.Factotum]: null,
-          [AssistantName.CollectionCreator]: null,
+      crypto.randomUUID().replaceAll("-", ""),
+    );
+    return { dataRepositoriesManager };
+  });
+});
+
+describe("InMemory", () => {
+  registerDataRepositoriesTests(() => {
+    const dataRepositoriesManager = new DemoDataRepositoriesManager(
+      {
+        appearance: { theme: Theme.Auto },
+        inference: {
+          chatCompletions: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+          },
+          transcriptions: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+          },
+          speech: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+            voice: null,
+          },
+          fileInspection: {
+            provider: { baseUrl: null, apiKey: null },
+            model: null,
+          },
+        },
+        assistants: {
+          userName: null,
+          developerPrompts: {
+            [AssistantName.Factotum]: null,
+            [AssistantName.CollectionCreator]: null,
+          },
         },
       },
-    },
-    crypto.randomUUID().replaceAll("-", ""),
-  );
-  return { dataRepositoriesManager };
+      crypto.randomUUID().replaceAll("-", ""),
+    );
+    return { dataRepositoriesManager };
+  });
 });
